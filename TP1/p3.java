775b829020d0521ff768acc77bd4d62e76e5d29c
VRMX2 <lehcengrissi@gmail.com>
2026-03-04 04:19:13 -0700

    new commit

--- TP1/p3.java
+++ TP1/p3.java
@@ -3,7 +3,7 @@ package TP1;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class p3 {//client
+public class p3 { //client
     public static void main(String[] args) {   
         try{
             Socket s = new Socket("localhost",1234);
